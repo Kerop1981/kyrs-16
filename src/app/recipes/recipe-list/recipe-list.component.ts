345c156f41187d9615e920c92589fe5fd88786f7
Kerop1981 <vova.abraamyan@mail.ru>
2024-04-25 16:50:09 +0300

    Передача данных с привязкой событий и свойств

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,7 +7,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+ @Output() recipeWasSelected = new EventEmitter<Recipe>()
 recipes: Recipe[] = [
-  new Recipe('a test recipe',' this is simply a test','https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/06/23/0/FNK_Goulash-H_s4x3.jpg.rend.hgtvcom.826.620.suffix/1592939451186.jpeg')
+  new Recipe('a test recipe',' this is simply a test','https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/06/23/0/FNK_Goulash-H_s4x3.jpg.rend.hgtvcom.826.620.suffix/1592939451186.jpeg'),
+  new Recipe('athoer test recipe',' this is simply a test','https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/06/23/0/FNK_Goulash-H_s4x3.jpg.rend.hgtvcom.826.620.suffix/1592939451186.jpeg')
 ];
+
+
+onRecipeSelected(recipe: Recipe){
+  this.recipeWasSelected.emit(recipe)
+}
 }
